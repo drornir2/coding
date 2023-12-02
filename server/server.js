72d8e3eb33487b50235681d8c66763564669cdfe
Chen Landau <chen201296@gmail.com>
2023-12-02 17:10:17 +0200

    deploy commit

--- server/server.js
+++ server/server.js
@@ -7,21 +7,25 @@ import http from "http";
 import cors from "cors";
 import { setUpIo } from "./services/SocketService.js";
 import codingRoomRouter from "./routes/codingRoomRouter.js";
+//public
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const server = http.createServer(app); //creating http server
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
-app.use("/api/v1/codingRoom", codingRoomRouter);
-
 setUpIo(server); // create SocketIO server
+app.use("/api/v1/codingRoom", codingRoomRouter);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-  res.send("server is running");
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
 });
-
 const port = process.env.PORT || 5000;
 
 try {
